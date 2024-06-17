9991dc81b55f75cadebca30f48e71642758e1acb
Jaiderdev <jaiderzambrano17dev@gmail.com>
2024-06-17 09:11:59 -0500

    Terminando

--- src/containers/sign-in/index.tsx
+++ src/containers/sign-in/index.tsx
@@ -1,10 +1,21 @@
-import { Button } from "@mui/material";
+import { Button, Container } from "@mui/material";
 import { Link } from "react-router-dom";
 
 export const SignIn = () => {
   return (
-    <Link to="assist">
-      <Button variant="outlined">Ir al chat</Button>
-    </Link>
+    <Container
+      sx={{
+        height: "100dvh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img src="/IAssist-logo.png" alt="logo" />
+      <Link to="assist">
+        <Button variant="outlined">Ir al chat</Button>
+      </Link>
+    </Container>
   );
 };
